10bf4b7895e8f80430fc47e072d2af3b7b0bf614
unknown <khadizakhanommeem@gmail.com>
2023-07-30 19:15:25 +0600

    test

---
+++ vite-project/src/App.vue
@@ -0,0 +1,100 @@
+<script setup>
+import{ref,reactive} from 'vue'
+import Questions from "./components/Questions.vue" 
+import Result from "./components/Result.vue" 
+
+const questions=reactive([
+  {
+    q:'1.what is 5+5?',
+    answers:[{
+      option:'10',
+      status: true
+    },
+    {
+      option:'11',
+    status: false
+  },
+  {
+    option:'12',
+    status: false
+  }]
+  },
+  {
+    q:'2.what is  1+1?',
+    answers:[{
+      option:'3',
+      status: false
+    },
+    {
+      option:'2',
+    status: true
+  },
+  {
+    option:'4',
+    status: false
+  }]
+  },
+  {
+    q:'3.what  is 4+6?',
+    answers:[{
+      option:'112',
+      status: false
+    },
+    {
+      option:'213',
+    status: false
+  },
+  {
+    option:'10',
+    status: true
+  }]
+  },
+  {
+    q:' 4.What is 9+0?',
+    answers:[{
+      option:'12',
+      status: false
+    },
+    {
+      option:'345',
+    status: false
+  },
+  {
+    option:'9',
+    status: true
+  }]
+  }
+])
+
+
+const correct=ref(0)
+
+
+
+function statusNo(status){
+  if(status){
+  correct.value++
+}
+  
+  }
+
+
+
+
+</script>
+
+<template>
+  <Questions
+  :questions="questions"
+
+  :correct="correct"
+  @statusNo="statusNo"
+ 
+  
+  />
+  <Result/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ vite-project/src/components/Questions.vue
@@ -0,0 +1,55 @@
+<script setup>
+import { ref,defineEmits,defineProps } from 'vue';
+
+const props= defineProps(['questions','index1','correct'])
+const emit=defineEmits(['statusNo'])
+function statusNo(status){
+    emit('statusNo',status)
+   
+}
+
+function reset(){
+  index1.value=0;
+
+
+}
+const index1=ref(0)
+
+
+</script>
+<template>
+    <div>
+        
+                <div 
+                    v-for="question,index in questions" 
+                    :key="index" 
+                    v-show="index1==index"
+                >
+
+                <div>
+                    {{ question.q }}
+                </div>
+            
+ 
+                <div  @click="statusNo(answer.status)" 
+
+                    v-for="answer,ind in question.answers" 
+
+                    :key="ind"
+                > 
+                    {{ answer.option }}
+                </div>
+            <!-- <div >{{ correct }}</div> -->
+            <div>Result: {{ correct }}</div>
+                </div><br>
+
+<button v-show="index1<questions.length-1" @click="index1++">next</button> <br>
+<button @click="reset" v-if="index1==questions.length-1">reset</button>
+
+
+<p v-if="index1==questions.length">{{ correct }}<button @click="reset">reset</button></p><br>
+<!-- <button></button> -->
+
+    </div>
+</template>
+<style scoped></style>
